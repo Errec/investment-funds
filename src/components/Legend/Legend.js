78942fc8eb28b5bfff987cffb1d35ce4bed066c7
Raniro <errecme@gmail.com>
2024-01-14 01:22:23 +0000

    Add element values

--- src/components/Legend/Legend.js
+++ src/components/Legend/Legend.js
@@ -1,13 +1,64 @@
 // Modules
 import React from 'react';
+import StarCircleIcon from 'mdi-react/StarCircleIcon';
+import InformationOutlineIcon from 'mdi-react/InformationOutlineIcon';
+import BlockHelperIcon from 'mdi-react/BlockHelperIcon';
+import ReplyCircleIcon from 'mdi-react/ReplyCircleIcon';
+import EarthPlusIcon from 'mdi-react/EarthPlusIcon';
 import { Grid, Cell } from 'react-foundation';
 
 // Styles
 import './Legend.sass';
+const legends = [
+    {
+        icon: StarCircleIcon,
+        description: 'Fundo para investidor qualificado',
+        color: '#639d31',
+        iconName: 'star',
+    },
+    {
+        icon: InformationOutlineIcon,
+        description: 'Entenda o resgate deste fundo',
+        color: '#505152',
+        iconName: 'info',
+    },
+    {
+        icon: BlockHelperIcon,
+        description: 'Fundo fechado para aplicação',
+        color: '#9c9d9e',
+        iconName: 'block',
+    },
+    {
+        icon: ReplyCircleIcon,
+        description: 'Aplicar neste fundo',
+        color: '#119c9f',
+        iconName: 'reply',
+    },
+]
 
 const Legend = () => {
     return (
         <section className="legend">
+            <Grid className="legend__wrapper">
+                <Cell className="legend__list" small={12} medium={9} large={9} isColumn>
+                    <p className="legend__title">
+                        <strong>LEGENDA</strong>
+                    </p>
+                    <Grid className="legend__list--1">
+                        {legends.map((item, index) => 
+                            <Cell className={"legend__item"} large={3} medium={3} small={12} isColumn key={index}>
+                                <item.icon className={`legend__icon legend__icon--${item.iconName}`} color={item.color}/>
+                                <span className={"legend__description"}>{item.description}</span>
+                            </Cell>)}
+                    </Grid>
+                    <Grid className="legend__list--2">
+                        <Cell className={"legend__item"} large={3} medium={3} small={12} isColumn>
+                            <EarthPlusIcon className={"legend__icon legend__icon--earth"} color={'#639d31'}/>
+                            <span className={"legend__description"}>Investimento ESG (Environmental, Social and Governance)</span>
+                        </Cell>
+                    </Grid>
+                </Cell>
+            </Grid>
         </section>
     );
 };
